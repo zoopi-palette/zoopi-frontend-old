6c4969b63b7692be6f449496da649cd6a788117c
jaehyeon(wiz) <eiirwp@gmail.com>
2022-04-14 23:15:45 +0900

    add theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import type {AppProps} from "next/app"
 import {GlobalStyle} from "styles/globalStyle"
+import {ThemeProvider} from "styles/theme"
 
 function MyApp({Component, pageProps}: AppProps) {
-  return (<>
-    <GlobalStyle/>
-    <Component {...pageProps} />
-  </>)
+  return (
+    <ThemeProvider>
+      <GlobalStyle/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
