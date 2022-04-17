f63dbf3017c1452b77bc9d24f042765ee909c73d
jaehyeon(wiz) <eiirwp@gmail.com>
2022-04-17 18:40:02 +0900

    add button with story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,32 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  // ref: https://github.com/storybookjs/storybook/issues/7540#issuecomment-797495306
+  webpackFinal: async (config) => {
+    config.module.rules[0].use[0].options.presets = [
+      require.resolve('@babel/preset-env'),
+      require.resolve('@babel/preset-typescript'),
+      [
+        require.resolve('@babel/preset-react'),
+        {
+          runtime: 'automatic',
+          importSource: '@emotion/react',
+        },
+      ],
+    ]
+
+    config.module.rules[0].use[0].options.plugins = [
+      ...config.module.rules[0].use[0].options.plugins,
+      '@emotion/babel-plugin',
+    ]
+
+    return config
+  },
 }
