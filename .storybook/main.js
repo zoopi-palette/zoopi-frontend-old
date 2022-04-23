10a0a8cb30e9eeca46afd46e367660db28d80019
jaehyeon(wiz) <eiirwp@gmail.com>
2022-04-24 02:34:22 +0900

    add more props in text-input

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
 module.exports = {
   "stories": [
     "../**/*.stories.@(js|jsx|ts|tsx)"
@@ -47,6 +49,13 @@ module.exports = {
         },
       }],
     });
+
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
 
     return config
   },
--- components/textInput/textInput.tsx
+++ components/textInput/textInput.tsx
@@ -1,15 +1,22 @@
 import {useTheme} from "@emotion/react"
-import {ChangeEventHandler, FocusEventHandler, MouseEventHandler, ReactNode, useCallback, useMemo} from "react"
-import {Css, CssObject, Theme} from "styles/theme"
+import {ChangeEvent, ChangeEventHandler, FocusEventHandler, ReactNode, useCallback, useEffect, useMemo, useRef, useState} from "react"
+import {Icon} from "components/icon"
+import {Css, CssObject} from "styles/theme"
 
 export type TextInputProps = {
   children?: ReactNode
   className?: string
   appearance?: ButtonAppearance
-  onClick?: ChangeEventHandler<HTMLInputElement> 
+  value: string
+  placeholder?: string
+  onChange?: (value: string, event?: ChangeEvent<HTMLInputElement>)=>void
   onBlur?: FocusEventHandler<HTMLInputElement>
   onFocus?: FocusEventHandler<HTMLInputElement>
   label?: string
+  autoFocus?: boolean
+  type?: "email" | "password" | "text";
+  clearDisabled?: boolean
+  right?: ReactNode
 }
 
 export type ButtonAppearance = "filled" | "outline"
@@ -17,11 +24,20 @@ export type ButtonAppearance = "filled" | "outline"
 export const TextInput = ({
   children,
   appearance = "filled",
-  onClick,
+  onChange,
+  onBlur,
+  onFocus,
+  value,
   label,
+  clearDisabled = false,
+  right,
   ...rest
 }: TextInputProps) => {
   const theme = useTheme()
+
+  const [localValue, setLocalValue] = useState("")
+  
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const css: Css = useMemo(()=>{
     return ({
@@ -42,35 +58,59 @@ export const TextInput = ({
   },[theme])
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = useCallback((event)=>{
-    onClick?.(event)
-  },[onClick])
+    setLocalValue(event.currentTarget.value)
+    onChange?.(event.currentTarget.value, event)
+  },[onChange])
 
   const handleBlur: FocusEventHandler<HTMLInputElement> = useCallback((event)=>{
-    onClick?.(event)
-  },[onClick])
+    onBlur?.(event)
+  },[onBlur])
 
   const handleFocus: FocusEventHandler<HTMLInputElement> = useCallback((event)=>{
-    onClick?.(event)
-  },[onClick])
+    onFocus?.(event)
+  },[onFocus])
 
+  const handleClearClick = useCallback(()=>{
+    setLocalValue("")
+    onChange?.("")
+  },[onChange])
+  
   return (
     <label css={{
       display: "inline-flex",
-      flexDirection: "column"
+      flexDirection: "column",
+      width: "100%",
+      position: "relative"
     }}>
       <span css={theme => ({
         fontSize: "0.875rem",
         color: theme.colors["grey-50"],
         marginBottom: "7px"
       })}>{label}</span>
-      <input 
+      <input
+        ref={inputRef}
         type={"text"}
         {...rest}
+        value={value || localValue}
         onChange={handleChange}
         onBlur={handleBlur}
         onFocus={handleFocus}
         css={css}
       />
+      <div css={{position: "absolute", right: 0, bottom: 6}}>
+        {right}
+        {clearDisabled ? null : (
+          <span
+            onClick={handleClearClick}
+            css={{
+              display: (value || localValue) ? "inline-flex" : "none",
+              cursor: "pointer"
+            }}
+          >
+            <Icon name={"close-circle"} color={theme.colors["grey-50"]}/>
+          </span>
+        )}
+      </div>
     </label>
   )
 }
