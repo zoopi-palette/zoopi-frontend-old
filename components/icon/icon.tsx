b5e2501aa5cdf590dc36b87a89042b260cbeb036
jaehyeon(wiz) <eiirwp@gmail.com>
2022-04-23 11:15:33 +0900

    add icon

---
+++ components/icon/icon.tsx
@@ -0,0 +1,43 @@
+import {useTheme} from "@emotion/react"
+import React from "react"
+import {ReactSVG} from "react-svg"
+
+export type IconProps = {
+  color?: string
+  name: IconName
+  size?: number
+}
+
+export type IconName = "arrow-back" | "arrow-right" | "check-circle" | "circle" | "close-circle" | "close" | "eye" | "search"
+
+export const Icon = ({
+  color,
+  size = 24,
+  name,
+}: IconProps) => {
+  const theme = useTheme()
+  
+  const handleBeforeInjection = (svg: SVGSVGElement) => {
+    if (!svg) return;
+
+    svg.setAttribute("width", size.toString());
+    svg.setAttribute("height", size.toString());
+  }
+
+  const reactSvgStyle = {
+    color: "inherit",
+    fontSize: 0,
+  }
+
+  return (
+    <span css={{
+      color: color ? color : theme.colors["grey-90"],
+    }}>
+      <ReactSVG
+        src={`/public/icons/${name}.svg`}
+        beforeInjection={(handleBeforeInjection)}
+        style={reactSvgStyle}
+      />
+    </span>
+  )
+}
