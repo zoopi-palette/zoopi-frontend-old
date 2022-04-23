c1724e060300380d60feef9249dd3bcb2b281416
jaehyeon(wiz) <eiirwp@gmail.com>
2022-04-23 15:58:42 +0900

    refactor replace react-svg by static import

--- components/icon/icon.tsx
+++ components/icon/icon.tsx
@@ -1,14 +1,33 @@
 import {useTheme} from "@emotion/react"
-import React from "react"
-import {ReactSVG} from "react-svg"
+import React, {ReactElement, SVGProps} from "react"
+
+import ArrowBack from "../../public/icons/arrow-back.svg"
+import ArrowRight from "../../public/icons/arrow-right.svg"
+import CheckCircle from "../../public/icons/check-circle.svg"
+import Circle from "../../public/icons/circle.svg"
+import CloseCircle from "../../public/icons/close-circle.svg"
+import Close from "../../public/icons/close.svg"
+import Eye from "../../public/icons/eye.svg"
+import Search from "../../public/icons/search.svg"
+
+const nameIconMap = {
+  "arrow-back": ArrowBack, 
+  "arrow-right": ArrowRight, 
+  "check-circle": CheckCircle, 
+  "circle": Circle, 
+  "close-circle": CloseCircle, 
+  "close": Close, 
+  "eye": Eye, 
+  "search": Search, 
+}
+
+export type IconName = keyof typeof nameIconMap
 
 export type IconProps = {
   name: IconName
   color?: string
   size?: number
 }
-
-export type IconName = "arrow-back" | "arrow-right" | "check-circle" | "circle" | "close-circle" | "close" | "eye" | "search"
 
 export const Icon = ({
   color,
@@ -17,27 +36,14 @@ export const Icon = ({
 }: IconProps) => {
   const theme = useTheme()
   
-  const handleBeforeInjection = (svg: SVGSVGElement) => {
-    if (!svg) return;
-
-    svg.setAttribute("width", size.toString());
-    svg.setAttribute("height", size.toString());
-  }
-
-  const reactSvgStyle = {
-    color: "inherit",
-    fontSize: 0,
-  }
+  const Icon = nameIconMap[name] as (props: SVGProps<SVGElement>) => ReactElement
 
   return (
     <span css={{
       color: color ? color : theme.colors["grey-90"],
+      fontSize: 0,
     }}>
-      <ReactSVG
-        src={`/icons/${name}.svg`}
-        beforeInjection={(handleBeforeInjection)}
-        style={reactSvgStyle}
-      />
+      <Icon width={size} height={size}/>
     </span>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    // ref: https://stackoverflow.com/a/61706308/11681543
+    const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test(".svg"));
+    fileLoaderRule.exclude = /\.svg$/;  
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: "pre",
+      use: [{
+        loader: "@svgr/webpack",
+        options: {
+          // ref: https://stackoverflow.com/a/71127131/11681543
+          svgoConfig: {
+            plugins: [
+              {removeViewBox: false},
+            ],
+          },
+        },
+      }],
+    });
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
