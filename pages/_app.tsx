b4db3009c0abbc82846129ccf62807cae1271ce4
hotbreakb <areyhappy12@gmail.com>
2022-04-28 17:33:01 +0900

    add provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
-import type {AppProps} from "next/app"
-import {GlobalStyle} from "styles/globalStyle"
-import {ThemeProvider} from "styles/theme"
+import type {AppProps} from "next/app";
+import {Provider} from "react-redux";
+import {store} from "modules/store";
+import {GlobalStyle} from "styles/globalStyle";
+import {ThemeProvider} from "styles/theme";
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
-    <ThemeProvider>
-      <GlobalStyle/>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+    <Provider store={store}>
+      <ThemeProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
