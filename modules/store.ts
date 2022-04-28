db4b78be5d055db063d113bb6bc29e53e9de57a5
hotbreakb <areyhappy12@gmail.com>
2022-04-28 13:09:14 +0900

    add store

--- modules/store.ts
+++ modules/store.ts
@@ -1,13 +1,24 @@
 import {configureStore} from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
 import {userSlice} from "./user/user";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const reducers = {
   user: userSlice.reducer,
+  // 리듀서 추가
 }
 
 export const store = configureStore({
   reducer: reducers,
+  middleware: [sagaMiddleware],
 });
+
+export function* rootSaga() {
+  // yield all([사가()]);
+}
+
+sagaMiddleware.run(rootSaga);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
