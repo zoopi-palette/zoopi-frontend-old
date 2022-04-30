e25651f306e6d4e51036dd7766ccc67d3699c7cc
hotbreakb <areyhappy12@gmail.com>
2022-05-01 08:33:54 +0900

    fix section

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -5,11 +5,13 @@ export const SearchBar = () => {
   const hrColor = "#DDDDDD";
 
   return (
-    <header
+    <section
       css={{
         height: 68,
         padding: 12,
         alignItems: "center",
+        display: "flex",
+        // justifyContent: "space-between",
       }}
     >
       <input type="text" placeholder="검색어를 입력해 주세요" />
@@ -17,6 +19,6 @@ export const SearchBar = () => {
       <Select id="distance" name="거리순"></Select>
       <Select id="distance" name="동물 선택"></Select>
       <Select id="distance" name="혈액형 선택"></Select>
-    </header>
+    </section>
   );
 };
