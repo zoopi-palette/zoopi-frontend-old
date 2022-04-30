267b3f2ef591197220633439150cf1b5186b1bef
hotbreakb <areyhappy12@gmail.com>
2022-04-30 11:27:22 +0900

    add stack component

---
+++ components/Stack/Stack.tsx
@@ -0,0 +1,56 @@
+import React, {ReactNode} from "react";
+
+// ref: https://stackoverflow.com/questions/62432985/typescript-saying-a-string-is-invalid-even-though-its-in-the-union
+type FlexDirection =
+  | "column"
+  | "inherit"
+  | "-moz-initial"
+  | "initial"
+  | "revert"
+  | "unset"
+  | "column-reverse"
+  | "row"
+  | "row-reverse"
+  | undefined;
+
+export type StackProps = {
+  children: ReactNode;
+  direction?: FlexDirection;
+  divider?: ReactNode;
+  spacing: number | string;
+};
+
+// ref: https://stackoverflow.com/questions/49496906/react-mapping-children-of-a-parent-component
+export const Stack = ({
+  children,
+  direction = "column",
+  divider,
+  spacing,
+}: StackProps) => {
+  return (
+    <>
+      {children && (
+        <div
+          css={{
+            display: "flex",
+            flexDirection: direction,
+            // marginBottom: spacing,
+            width: 400,
+            padding: 24,
+            overflowY: "scroll",
+            // flex: 1,
+          }}
+        >
+          {React.Children.map(children, (child) => {
+            return (
+              <>
+                {child}
+                {divider}
+              </>
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
+};
