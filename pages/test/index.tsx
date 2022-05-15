54e7349a569bd5c19d62917bf66cf4e5fd509ebe
jaehyeon(wiz) <eiirwp@gmail.com>
2022-05-15 15:20:38 +0900

    add test modal in test page

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,15 +1,15 @@
-import {useTheme} from "@emotion/react";
 import type {NextPage} from "next"
 import {LayoutCenter} from "components/LayoutCenter"
+import {ModalLinkSent} from "components/ModalLinkSent";
+import {useModal} from "utils/modal";
 
 
 const Test: NextPage = () => {
-  const theme = useTheme();
+  const {toggleModal} = useModal(ModalLinkSent, {})
   
-
   return (
     <LayoutCenter>
-      
+      <button onClick={()=>toggleModal(true)}>open</button>
     </LayoutCenter>
   )
 }
